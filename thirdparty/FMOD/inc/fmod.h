9a9d32122cceb45213b34d3498f604e56d095b18
alexey.lysiuk <alexey.lysiuk@gmail.com>
2015-12-30 13:36:08 +0200

    * update FMOD Ex to version 4.44.59

--- thirdparty/FMOD/inc/fmod.h
+++ thirdparty/FMOD/inc/fmod.h
@@ -16,7 +16,7 @@
     0xaaaabbcc -> aaaa = major version number.  bb = minor version number.  cc = development version number.
 */
 
-#define FMOD_VERSION    0x00044449
+#define FMOD_VERSION    0x00044459
 
 /*
     Compiler specific settings.
